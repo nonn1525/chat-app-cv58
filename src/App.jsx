c190d914fdcdd54c4b02757912d9179d23952d34
Chisato Kato <i.like.nuko@gmail.com>
2021-06-13 19:53:43 +0900

    表示されないバグ修正途中

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import { auth } from './config/firebase';
-import { Login, Signup, Room } from './pages'; // まとめてimportできるのはなぜか？
-import { Auth, LoggedInRoute } from './utils';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
+// import { auth } from './config/firebase';
+// import { Login, Signup, Room } from './pages'; // まとめてimportできるのはなぜか？
+// import { Auth, LoggedInRoute } from './utils';
 
 function App() {
   // console.log(process.env.REACT_APP_HELOO); // .envの確認
 
   return (
-    <Router>
+    // <Router>
       <h1>Chat App</h1>
-      <ul> {/* 確認用のメニュー */}
-        <li><Link to='/'>ROOM</Link></li>
-        <li><Link to='/login'>LOGIN</Link></li>
-        <li><Link to='/signup'>SIGNUP</Link></li>
-      </ul>
-      <button onClick={() => auth.signOut()}>logout</button>
-      <Auth>
-        <Switch>
-          <LoggedInRoute exact path='/' component={Room} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/signup' component={Signup} />
-        </Switch>
-      </Auth>
-    </Router>
+    //   <ul> {/* 確認用のメニュー */}
+    //     <li><Link to='/'>ROOM</Link></li>
+    //     <li><Link to='/login'>LOGIN</Link></li>
+    //     <li><Link to='/signup'>SIGNUP</Link></li>
+    //   </ul>
+    //   <button onClick={() => auth.signOut()}>logout</button>
+    //   <Auth>
+    //     <Switch>
+    //       <LoggedInRoute exact path='/' component={Room} />
+    //       <Route exact path='/login' component={Login} />
+    //       <Route exact path='/signup' component={Signup} />
+    //     </Switch>
+    //   </Auth>
+    // </Router>
   );
 }
 
